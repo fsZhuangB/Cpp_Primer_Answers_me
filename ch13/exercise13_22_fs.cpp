a994b112e315f0e35f887595a328cd6807ce95d7
fszhuangb <fszhuangb@gmail.com>
2020-06-08 22:52:46 -0400

    add all the exercise of hasptr class

---
+++ ch13/exercise13_22_fs.cpp
@@ -0,0 +1,50 @@
+#include <string>
+using namespace std;
+
+class HasPtr {
+public:
+	HasPtr(const string& s = string()) :
+		 ps(new string(s)), i(0) { }
+
+	// the constructor should dynamically allocate a new string
+	// and copy the object to which ps points, rather copying ps itself
+	HasPtr(const HasPtr& hpr) : ps(new string(*hpr.ps)), i(hpr.i) { }
+	
+
+	// Your assignment operator should copy the object to which ps points
+	HasPtr& operator=(const HasPtr& hpr)
+	{
+		string* new_hasptr = new string(*hpr.ps);
+		delete ps;
+		ps = new_hasptr;
+		i = hpr.i;
+		return *this;
+	}
+
+
+	// remember the synthesized destructor won't 
+	// delete a data member that is a pointer
+	~HasPtr()
+	{
+		delete ps;
+	}
+
+private:
+	string* ps;
+	int i;
+};
+
+HasPtr f(HasPtr hp)
+{
+	HasPtr ret = hp;
+	return ret;
+}
+int main()
+{
+	HasPtr a("hhhh");
+	f(a);
+	HasPtr q(a);
+
+	return 0;
+	
+}
